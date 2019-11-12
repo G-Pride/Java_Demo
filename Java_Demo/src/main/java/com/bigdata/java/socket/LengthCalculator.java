cbbeb691d704f83224771668d042845a5f2eda17
G-Pride <493536604@qq.com>
2019-11-12 13:47:01 +0800

    socket实例

---
+++ Java_Demo/src/main/java/com/bigdata/java/socket/LengthCalculator.java
@@ -0,0 +1,53 @@
+/**
+   guozh
+ * 2019年11月8日
+ */
+package com.bigdata.java.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * @author guozh
+ *
+ * 创建时间：2019年11月8日 下午3:27:45
+ */
+public class LengthCalculator extends Thread{
+
+	private Socket socket;
+	/**
+	 * @param socket
+	 */
+	public LengthCalculator(Socket socket) {
+		this.socket  = socket;
+	}
+
+	/**
+	 * 
+	 */
+	public void run() {
+		try {
+			OutputStream os  = socket.getOutputStream();//输出流
+			InputStream is =  socket.getInputStream();//输入流
+			int ch =0;
+			byte[] buff = new byte[1024];
+			//buff主要用来读取输入的内容，存成byte数组，ch主要用来获取读取数组的长度
+			ch = is.read(buff);
+			//将接收流的byte数组转换成字符串，这里获取的内容是客户端发送过来的字段
+			String content  = new String(buff, 0, ch);
+			System.out.println(content);
+			//往输出流里写入获得字符串的长度，回发给客户端
+			os.write(String.valueOf(content.length()).getBytes());
+			
+			is.close();
+			os.close();
+			socket.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ Java_Demo/src/main/java/com/bigdata/java/socket/TCPServer.java
@@ -0,0 +1,32 @@
+/**
+   guozh
+ * 2019年11月8日
+ */
+package com.bigdata.java.socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.jayway.jsonpath.internal.function.text.Length;
+
+/**
+ * @author guozh
+ *
+ * 创建时间：2019年11月8日 下午3:19:59
+ */
+public class TCPServer {
+	
+	public static void main(String[] args) throws IOException {
+		//创建socket，并将socket绑定到65000端口
+		ServerSocket ss = new ServerSocket(65000);
+		//死循环，使socket一直等待并处理客户端发送过来的请求
+		while(true) {
+			//监听65000端口，直到客户端返回连接信息后才返回
+			Socket socket = ss.accept();
+			//获取客户端的请求信息后，执行相关的业务逻辑
+			new LengthCalculator(socket).start();
+		}
+	}
+
+}
